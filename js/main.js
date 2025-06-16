cf24e9ba4252ef283412c1345be6d64e1080ed8f
Meryem İnalkaç <meryeminalkac80@gmail.com>
2025-06-16 23:49:28 +0300

    yine url

--- js/main.js
+++ js/main.js
@@ -155,11 +155,12 @@ $(document).ready(function() {
             }
             
             // API isteği gönder
-            const response = await fetch("https://formul-backend.onrender.com/send-form",{
+            const response = await fetch("https://formul-backend.onrender.com/send-form", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
-            });
+              });
+              
             
             // Yanıtı işle
             if (!response.ok) {
